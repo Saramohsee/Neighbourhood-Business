eef3c72e97accf21e63ac1db3ea6069b2ba71374
Sara_Mohsen <sara.mohsen@education.nsw.gov.au>
2023-09-07 18:20:24 +1000

    littlestuff

---
+++ my-app/src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './Header.module.css'
+
+export default function Header() {
+    return (
+        <div>
+            <h1 className={style.Heading}>Sara's Sitters Hornsby</h1>
+
+        </div>
+    )
+}
---
+++ my-app/src/Pages/Home/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './home.module.css'
+import {Header} from '../../Components/Header/Header'
+
+export default function home() {
+    return (
+      <div>
+       <Header/>
+      </div>
+    )
+}
